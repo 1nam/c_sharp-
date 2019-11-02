a8b81c3d2b11f652b51d6ecbe1c9f77148263e2f
zeus <33592162+1nam@users.noreply.github.com>
2019-11-02 01:21:58 -0400

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,34 @@
+// i am new to c#
+// this is how i was showen to do it.
+//...............................................................
+// these // hide text in your script
+// amd tell other developers something about the script
+// this should help get started if you know a better way share.
+//.............................................................
+// using System = its what your script needs to run.
+// name speace = is the name you gave the script.
+// class = is the program.
+// static void main = is where you write your code.
+// ...........................................................
+// match curly braces vertical 1 and 1 , 2 and 2 , 3 and 3
+// using simply 1234 placing a cyrly brace on the 5th
+// ...........................................................
+// curly brace 1 dont count {
+// curly brace 2 count spaces 1234{
+// curly brace 3 count spaces 12341234{
+// curly brace 3 count spaces 12341234{
+// curly brace 2 count spaces 1234{
+// curly brace 1 dont count {
+//............................................................
+using System;
+
+namespace ConsoleApp1
+{//1
+    class Program
+    {//2
+        static void Main(string[] args)
+        {//3
+            Console.WriteLine("Hello!");
+        }//3
+    }//2
+}//1
